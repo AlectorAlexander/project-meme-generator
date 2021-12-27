eeaaef3e59f97c388e6c60a6825b5d82da23363c
Aléctor Alexander <alector.alexandre@gmail.com>
2021-12-27 20:00:30 -0300

    projeto pronto

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 
 
-let textoInsert = document.getElementById("text-input")
+let textoInsert = document.querySelector("#text-input")
 let campoTexto = document.getElementById("meme-text")
 
-textoInsert.addEventListener("keyup", function () {
-    if (event.keyCode === 13){
-    insereTexto()
-}} )
+textoInsert.addEventListener("keyup", insereTexto)
 
 function insereTexto () {
    let valorInput = textoInsert.value
-   textoInsert.value = '' 
 let h1 = document.createElement("p")
 h1.innerText = valorInput
 
 campoTexto.appendChild(h1)
+if (campoTexto.childElementCount > 1) {
+    let apagar = campoTexto.children[0]
+    apagar.parentNode.removeChild(apagar)
+
+}
 }
 
 let memeInsert = document.getElementById("meme-insert")
@@ -23,8 +24,9 @@ memeInsert.addEventListener("change", insereImagem)
 
 //Referência que sem ela eu não seria nada: https://www.youtube.com/watch?v=VElnT8EoEEM
 function insereImagem () {
+    console.log(memeInsert.value)
     const file = memeInsert.files[0]
-    let imageContainer = document.getElementById("meme-image-container")
+    let imageContainer = document.getElementById("meme-image")
     const meme = imageContainer.querySelector("#meme")
 
 
@@ -43,6 +45,66 @@ function insereImagem () {
 }
 
 
+}
+
+let fire = document.getElementById("fire")
+let water = document.getElementById("water")
+let earth = document.getElementById("earth")
+
+fire.addEventListener("click", fogo)
+water.addEventListener("click", agua)
+earth.addEventListener("click", terra)
+let imageContainer = document.getElementById("meme-image-container")
+
+
+function fogo () {
+    
+    imageContainer.style.border = "3px dashed red"
+}
+function agua () {
+    imageContainer.style.border = "5px double blue"
+
+
+}
+function terra () {
+    imageContainer.style.border = "6px groove green"
+
+}
+
+
+
+
+
+
+
+let cMeme = document.querySelector("#préMemes")
+for (let meme = 0; meme < cMeme.childElementCount; meme++) {
+    const element = cMeme.children[meme];
+    element.addEventListener("click", insereMeme)
+}
+
+function insereMeme (mm) {
+    let imageMeme = document.getElementById("meme-image")
+    const memeId = imageMeme.querySelector("#meme")
+
+const id = mm.target
+if (id.id == "meme-1"){
+    memeId.style.display = "block"
+    memeId.setAttribute("src", 'imgs/meme1.png')
+} else if (id.id == "meme-2"){
+    memeId.style.display = "block"
+    memeId.setAttribute("src", 'imgs/meme2.png')
+
+}else if (id.id == "meme-3"){
+    memeId.style.display = "block"
+    memeId.setAttribute("src", 'imgs/meme3.png')
+
+
+}else if (id.id == "meme-4"){
+    memeId.style.display = "block"
+    memeId.setAttribute("src", 'imgs/meme4.png')
+
+}
 }
 
 
